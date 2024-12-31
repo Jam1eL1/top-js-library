d144fee09280553e0459f1e454df1cbe4e859c08
Jam1eL1 <23jisu@gmail.com>
2024-12-31 15:51:37 +0900

    style: center the main content

--- script.js
+++ script.js
@@ -29,7 +29,9 @@ function displayBooks() {
         libraryContainer.appendChild(bookCard);
     });  
 }
-
+// test
 addBookToLibrary(new Book('The Hobbit', 'J.R.R. Tolkien', '700','Y', 4.5));
 addBookToLibrary(new Book('1984', 'George Orwell', '200', 'N', 4.8));
 addBookToLibrary(new Book('Pride and Prejudice', 'Jane Austen', '1000', 'Y', 4.7));
+
+displayBooks();
