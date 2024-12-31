56635f4bb8c29390db154d7e65f8d6099e9c3a60
Jam1eL1 <23jisu@gmail.com>
2024-12-31 15:19:57 +0900

    feat: create displayBooks function that creates bookCard divs while iterating myLibrary and add to the libraryContainer in HTML

--- script.js
+++ script.js
@@ -19,6 +19,17 @@ function displayBooks() {
     myLibrary.forEach((book)=> {
         const bookCard = document.createElement('div');
         bookCard.classList.add('book-card');
-    }
-    );  
+        bookCard.innerHTML = `
+            <h3>${book.title}</h3>
+            <p>By: ${book.author}</p>
+            <p>Pages: ${book.pages}</p>
+            <p>Read: ${book.read}</p>
+            <p>Rating: ${book.ratings}</p>
+        `;
+        libraryContainer.appendChild(bookCard);
+    });  
 }
+
+addBookToLibrary(new Book('The Hobbit', 'J.R.R. Tolkien', '700','Y', 4.5));
+addBookToLibrary(new Book('1984', 'George Orwell', '200', 'N', 4.8));
+addBookToLibrary(new Book('Pride and Prejudice', 'Jane Austen', '1000', 'Y', 4.7));
