2a1da50f90da978966680e55945358ca9118b09a
Jam1eL1 <23jisu@gmail.com>
2024-12-31 17:39:15 +0900

    feat: add eventListeners to show user input modal when '+' button is clicked to add new book

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 const myLibrary = [];
+const modal = document.getElementById('modal');
+const addBook = document.getElementById('add-book');
+const saveBook = document.getElementById('save-book');
+const cancelModal = document.getElementById('cancel-modal');
+
+addBook.addEventListener('click', () => {
+    modal.showModal();
+});
+
+saveBook.addEventListener('click', () => {
+    modal.close();
+});
+
+cancelModal.addEventListener('click', () => {
+    modal.close();
+});
+
 
 function Book(title, author, pages, read, ratings) {
   this.title = title;
@@ -15,7 +32,7 @@ function addBookToLibrary(book) {
 function displayBooks() {
     const libraryContainer = document.getElementById('library-container');
     libraryContainer.innerHTML = '';
-    
+
     myLibrary.forEach((book)=> {
         const bookCard = document.createElement('div');
         bookCard.classList.add('book-card');
@@ -34,4 +51,4 @@ addBookToLibrary(new Book('The Hobbit', 'J.R.R. Tolkien', '700','Y', 4.5));
 addBookToLibrary(new Book('1984', 'George Orwell', '200', 'N', 4.8));
 addBookToLibrary(new Book('Pride and Prejudice', 'Jane Austen', '1000', 'Y', 4.7));
 
-displayBooks();
+
