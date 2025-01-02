e412f939f52e6bfa56cf71c9b52c42c0ef5a30eb
Jam1eL1 <23jisu@gmail.com>
2025-01-02 15:03:37 +0900

    feat: add event listener to bookForm for dialog submission

--- script.js
+++ script.js
@@ -3,8 +3,25 @@ const bookDialog = document.getElementById('book-dialog');
 const addBookBtn = document.getElementById('add-book');
 const saveBookBtn = document.getElementById('save-book');
 const cancelBookBtn = document.getElementById('cancel-book');
+
 const bookForm = document.getElementById('book-form');
-bookForm.addEventListener('submit')
+bookForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const title = document.getElementById('book-title').value;
+    const author = document.getElementById('book-author').value;
+    const pages = document.getElementById('book-pages').value;
+    const readStatus = document.querySelector('input[name="read-status"]:checked').value;
+    const rating = document.getElementById('book-rating').value;
+
+    console.log({
+        title,
+        author,
+        pages,
+        readStatus,
+        rating
+    });
+}); 
+
 addBookBtn.addEventListener('click', () => {
     bookDialog.showModal();
 });
@@ -23,9 +40,9 @@ function Book(title, author, pages, read, ratings) {
   this.ratings = ratings;
 }
 
-function addNewBook() {
+// function addNewBook() {
 
-}
+// }
 
 function addBookToLibrary(book) {
   myLibrary.push(book);
